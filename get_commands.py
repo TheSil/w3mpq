4d4d5b6a2320e35c7700efc99c30d3aaa463d705
TheSil <sildak@gmail.com>
2020-03-08 18:27:16 +0100

    added __main__ added protection against non-standard unicode characters

--- get_commands.py
+++ get_commands.py
@@ -3,19 +3,23 @@ import pprint
 import re
 import sys
 
-filename = sys.argv[1]
-archive = MPQArchive(filename, listfile=False)
-script = archive.read_file('war3map.j')
-if not script:
-    script = archive.read_file(r'Scripts\war3map.j')
-script = script.decode('utf-8')
+def get_commands(filename):
+    archive = MPQArchive(filename, listfile=False)
+    script = archive.read_file('war3map.j')
+    if not script:
+        script = archive.read_file(r'Scripts\war3map.j')
+    script = script.decode('utf-8', errors='replace')
 
-re_str = r'call TriggerRegisterPlayerChatEvent\([^,]*,([^,]*),[ ]*"([^"]*)"[ ]*,[^,]*\)'
+    re_str = r'call TriggerRegisterPlayerChatEvent\([^,]*,([^,]*),[ ]*"([^"]*)"[ ]*,[^,]*\)'
 
-cmds =  dict()
-for player, cmd in re.findall(re_str, script):
-    if cmd not in cmds:
-        cmds[cmd] = []
-    cmds[cmd].append(player.strip())
+    cmds =  dict()
+    for player, cmd in re.findall(re_str, script):
+        if cmd not in cmds:
+            cmds[cmd] = []
+        cmds[cmd].append(player.strip())
 
-pprint.pprint(cmds)
+    return cmds
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    pprint.pprint(get_commands(filename))
